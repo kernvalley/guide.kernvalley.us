493feef0c78e557ec4c499fd13afef5e528ad7a0
Chris Zuber <shgysk8zer0@gmail.com>
2023-05-15 23:32:47 -0700

    Misc updates

--- js/components.js
+++ js/components.js
@@ -1,6 +1,6 @@
 import '@shgysk8zer0/components/share-button.js';
 import '@kernvalley/components/events.js';
-import '@shgysk8zer0/components/share-to-button/share-to-button.js';
+import '@shgysk8zer0/components/button/share-to.js';
 import '@shgysk8zer0/components/github/user.js';
 import '@shgysk8zer0/components/current-year.js';
 import '@shgysk8zer0/components/install/prompt.js';
